45cfbf6c209a97aaab3eb9e8d8dabaaf329c3e6f
Spencer Shaw <sshaw32@gatech.edu>
2016-03-12 16:20:10 -0500

    Imports wave file and gets its samplewidth

--- sample.py
+++ sample.py
@@ -1,1 +1,12 @@
 #lsdkfjslkdfjlskdfjlskdfjlkfdj
+
+import wave
+
+print ("Hello World")
+
+def main():
+	waveObject = wave.open('test_mono_8000Hz_16bit_PCM.wav', 'r')
+	width = waveObject.getsamplewidth()
+	print (width)
+	
+main()
